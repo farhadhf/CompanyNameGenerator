da618afe2a0bfd052b288b34b7cee06cd1c14870
Farhad Hedayatifard <hf.farhad@gmail.com>
2017-04-08 14:23:02 +0430

    Use \Faker\Provider\Base::randomElements instead of \Faker\Provider\Base::randomElement and cleanup the code.

--- test.php
+++ test.php
@@ -7,11 +7,7 @@ $faker->addProvider(new CompanyNameGenerator\FakerProvider($faker));
 $i = 0;
 while ($i < 20) {
     $name = $faker->companyName;
-    if (!checkdnsrr('www.' . $name . '.com', 'ANY') && !checkdnsrr($name . '.com', 'ANY')) {
-        echo "\n", $name;
-        $i++;
-    } else {
-        echo '.';
-    }
+    echo "\n", $name;
+    $i++;
 }
 echo "\n";
